006ca2a34df2c2c0054576b7bf39bdea1e1157a3
Sean <seanbopo@gmail.com>
2023-07-10 11:09:44 +0800

    Day 17: Project 1 - WeSplit

---
+++ Project01-WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Po Hsiang Chao on 2023/7/9.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
