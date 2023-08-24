1dee5893e6a08d9df40e82141373d7e7f36c68ad
Sean <seanbopo@gmail.com>
2023-08-24 17:12:05 +0800

    Day 51: Project 10 about Cupcake Corner. Network function.

--- 14-Project10-CupcakeCorner/CupcakeCorner/CupcakeCorner/Views/CheckoutView.swift
+++ 14-Project10-CupcakeCorner/CupcakeCorner/CupcakeCorner/Views/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -25,13 +28,44 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place Order", action: { })
+                Button("Place Order", action: {
+                    Task {
+                        await placeOrder()
+                    }
+                })
                     .padding()
 
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            // handle the result
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
     }
 }
 
--- 14-Project10-CupcakeCorner/README.md
+++ 14-Project10-CupcakeCorner/README.md
@@ -2,6 +2,7 @@
 
 - [Part 1](https://www.hackingwithswift.com/100/swiftui/49)
 - [Part 2](https://www.hackingwithswift.com/100/swiftui/50)
+- [Part 3](https://www.hackingwithswift.com/100/swiftui/51)
 
 ## **Learn**
 
@@ -22,11 +23,18 @@
     - **disabled()**
     - **AsyncImage**
     
+- Day 51
+    - **encode / decode**
+    - Task
+    - URLRequest
+    - URLSession.shared.upload
+    
     
 ## **My Note**
 
 - [Day 49](https://hsiangdev.notion.site/Day-49-Project-10-Part-1-100DaysOfSwiftUI-34a7edbc30a240ae9ae5856f7d2ce7ab?pvs=4)
 - [Day 50](https://hsiangdev.notion.site/Day-50-Project-10-Part-2-CupcakeCorner-100DaysOfSwiftUI-d308b7db023f42f5bcdf719b27506f34?pvs=4)
+- [Day 51](https://hsiangdev.notion.site/Day-51-Project-10-Part-3-CupcakeCorner-100DaysOfSwiftUI-9b43d50d4de045259e3f1bd56303fffe?pvs=4)
 
 ## Screenshots
 
@@ -36,5 +44,12 @@
     <img src="Screenshots/day50-CupcakeCorner-1.png" width="250">
     <img src="Screenshots/day50-CupcakeCorner-2.png" width="250">
     <img src="Screenshots/day50-CupcakeCorner-3.png" width="250">
+</div>
+
+- Day 51
+
+<div>
+    <img src="Screenshots/day51-CupcakeCorner-1.png" width="250">
+    <img src="Screenshots/day51-CupcakeCorner-2.png" width="250">
 </div>
 
